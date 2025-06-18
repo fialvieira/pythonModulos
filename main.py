4ead8a5a7c8fad8752d298f9d19ba1587fb0c37b
fialvieira <fialvieira@gmail.com>
2025-06-18 20:37:28 -0300

    Módulos em Python.

---
+++ main.py
@@ -0,0 +1,15 @@
+# Programa principal
+import string_utils as su
+import math_operations as math
+from math_operations import multiply, divide
+
+print(math.sum(5, 3))
+print(math.subtract(5, 3))
+print(multiply(4, 2))
+print(divide(4, 2))
+
+print("Funções String")
+
+print(su.capitalize("hello world!"))
+print(su.reverse_string("Python"))
+print(su.count("Apple"))
